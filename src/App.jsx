343018f48ba17fd2e8615bd5a8e2883e341c06f7
KaveenKrithik <141116910+KaveenKrithik@users.noreply.github.com>
2024-12-01 11:01:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import ItemDetailsPage from './pages/ItemDetailsPage';
+import LoginPage from './components/LoginPage'; 
 
 function App() {
   return (
     <Router>
       <div>
-        <Header /> {}
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/item/:itemName" element={<ItemDetailsPage />} />
+          <Route path="/login" element={<LoginPage />} /> {}
         </Routes>
       </div>
     </Router>
@@ -19,6 +21,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
